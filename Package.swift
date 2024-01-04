4df2d5356fa8b3401c992762b9b674b57f3a1731
Ambas <ambaschbsanti@gmail.com>
2024-01-04 13:53:33 +0700

    Add public

--- Package.swift
+++ Package.swift
@@ -5,19 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUICoreLocation",
+    platforms: [.iOS(.v17), .macOS(.v14)], 
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwiftUICoreLocation",
-            targets: ["SwiftUICoreLocation"]),
+      // Products define the executables and libraries a package produces, making them visible to other packages.
+      .library(
+        name: "SwiftUICoreLocation",
+        targets: ["SwiftUICoreLocation"])
     ],
+    dependencies: [
+            .package(url: "https://github.com/AsyncSwift/AsyncLocationKit", from: "1.6.4"),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftUICoreLocation"),
+            name: "SwiftUICoreLocation",
+            dependencies: ["AsyncLocationKit"]),
         .testTarget(
             name: "SwiftUICoreLocationTests",
             dependencies: ["SwiftUICoreLocation"]),
     ]
 )
+// AsyncLocationKit
--- Sources/SwiftUICoreLocation/SwiftUICoreLocation.swift
+++ Sources/SwiftUICoreLocation/SwiftUICoreLocation.swift
@@ -1,2 +1,61 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import AsyncLocationKit
+import SwiftUI
+import MapKit
+
+public struct EquatableError: Error, Equatable {
+    let base: Error
+  
+  init(_ base: Error) {
+    self.base = base
+  }
+
+    public static func ==(lhs: EquatableError, rhs: EquatableError) -> Bool {
+        return type(of: lhs.base) == type(of: rhs.base) &&
+        lhs.base.localizedDescription == rhs.base.localizedDescription
+    }
+}
+
+public enum LocationEvent: Equatable {
+  case initial
+  case paused
+  case resume
+  case update(locations: [CLLocation])
+  case fail(EquatableError)
+}
+
+public struct LocationManager: ViewModifier {
+  let requestPermission: LocationPermission
+  let manager: AsyncLocationManager
+  @Binding var event: LocationEvent
+  
+  public init(requestPermission: LocationPermission, event: Binding<LocationEvent>) {
+    self.requestPermission = requestPermission
+    _event = event
+    manager = AsyncLocationManager(desiredAccuracy: .bestAccuracy)
+  }
+  
+  public func body(content: Content) -> some View {
+    content
+      .task {
+        let _ = await manager.requestPermission(with: requestPermission)
+        for await event in await manager.startUpdatingLocation() {
+          switch event {
+          case .didPaused:
+            self.event = .paused
+          case .didResume:
+            self.event = .resume
+          case .didUpdateLocations(locations: let locations):
+            self.event = .update(locations: locations)
+          case .didFailWith(error: let error):
+            self.event = .fail(.init(error))
+          }
+        }
+      }
+  }
+}
+
+extension View {
+  func locationManager(requestPermission: LocationPermission, event: Binding<LocationEvent>) -> some View {
+    modifier(LocationManager(requestPermission: requestPermission, event: event))
+  }
+}
